bf78a79ed40f4be411f684fdbc5bdf08e4950f6d
Niccolò Maltoni <niccolo.maltoni@diennea.com>
2024-04-04 09:09:12 +0200

    ci: add JMH action

--- src/main/java/it/niccomlt/diennea/smtp/BenchmarkRunner.java
+++ src/main/java/it/niccomlt/diennea/smtp/BenchmarkRunner.java
@@ -2,6 +2,7 @@ package it.niccomlt.diennea.smtp;
 
 import it.niccomlt.diennea.smtp.benchmarks.MultiMessageDeliveryBenchmark;
 import it.niccomlt.diennea.smtp.benchmarks.SingleMessageDeliveryBenchmark;
+import org.openjdk.jmh.results.format.ResultFormatType;
 import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
@@ -9,13 +10,15 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
  * Entrypoint of the benchmarks.
  */
 public class BenchmarkRunner {
-    public static void main(String[] args) throws Exception {
-        var opt = new OptionsBuilder()
+    public static void main(final String[] args) throws Exception {
+        final var opt = new OptionsBuilder()
             .include(SingleMessageDeliveryBenchmark.class.getSimpleName())
             .include(MultiMessageDeliveryBenchmark.class.getSimpleName())
             .shouldFailOnError(true)
+            .result("output.json")
+            .resultFormat(ResultFormatType.JSON)
+            .output("output.log")
             .build();
-
         new Runner(opt).run();
     }
 }
